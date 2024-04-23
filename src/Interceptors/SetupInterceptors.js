07d767aa08e84a98c52265a9d34813fafc755320
Sashok9203 <kovalchuk.olex@gmail.com>
2024-04-23 16:10:25 +0300

    Update

--- src/Interceptors/SetupInterceptors.js
+++ src/Interceptors/SetupInterceptors.js
@@ -24,8 +24,8 @@ axios.interceptors.response.use(
             break; 
   
         default: {
-          const location =   window.location.pathname.slice(1);
-          window.location = `/error/${status}/${status}/${error.message}/${location ===''?'main':location}`;
+        //  const location =   window.location.pathname.slice(1);
+        //  window.location = `/error/${status}/${status}/${error.message}/${location ===''?'main':location}`;
         // message.error(error.message)
           return Promise.reject(error.message);
         }
