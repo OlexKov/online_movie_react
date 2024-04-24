7a3c74d94fe82d7c7946477dd2a4ca1829ea8a5d
Sashok9203 <kovalchuk.olex@gmail.com>
2024-04-25 00:10:53 +0300

    Fix

--- src/Interceptors/SetupInterceptors.js
+++ src/Interceptors/SetupInterceptors.js
@@ -1,40 +1,30 @@
 import axios from 'axios';
-import {message } from 'antd'
+import { message } from 'antd'
 
 export const SetupInterceptors = () => {
-    axios.interceptors.request.use((config) => {
-    return config;
-}, (error) => {
-   
-   message.error(`${error.status} ${error.message}`)
-   return Promise.reject(error);
-});
+  axios.interceptors.request.use(
+    config => config,
+    (error) => {
+      message.error(`${error.status} ${error.message}`)
+      return Promise.reject(error);
+    });
 
-axios.interceptors.response.use(
-    (response) => {
-      
-      return response
-    },
-    ( error) => {
-     
+  axios.interceptors.response.use(
+    response => response,
+    (error) => {
       const status = error.response?.status || 500;
-      
       switch (status) {
-    
+
         // case 401:
         //     break; 
-  
+
         default: {
           //const location =   window.location.pathname.slice(1);
           //window.location = `/error/${status}/${status}/${error.message}/${location ===''?'main':location}`;
           message.error(error.message)
-
           return Promise.reject(error.message);
         }
       }
     }
   );
-
-
-
 }
